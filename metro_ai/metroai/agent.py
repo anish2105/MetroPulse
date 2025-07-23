d6b6fe2aadcffc4291d7ca4b9e86c389d9d9f116
poornachandra24 <iris24244242@gmail.com>
2025-07-24 02:59:03 +0530

    refactor(#13): filenaming conventions updated to cloud run compatibility

--- metro_ai/metroai/agent.py
+++ metro_ai/metroai/agent.py
@@ -4,8 +4,8 @@ import os
 load_dotenv()
 from google.adk.agents import Agent
 
-from metro_agent.prompt import ROOT_AGENT_INSTRUCTION
-from metro_agent.tools.character_counter import count_characters
+from metroai.prompt import ROOT_AGENT_INSTRUCTION
+from metroai.tools.character_counter import count_characters
 
 root_agent = Agent(
     name="metro_agent",
